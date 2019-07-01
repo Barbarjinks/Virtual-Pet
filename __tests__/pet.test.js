214c9a4ccf6662e75ceb227183016391710858eb
Cai Jenkins <Caisjenkins@gmail.com>
2019-07-01 21:21:10 +0100

    added grow up

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,17 +2,17 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
- expect(new Pet('Fido')).toBeInstanceOf(Object);
+ expect(new Pet('Eric')).toBeInstanceOf(Object);
     });
 
     it('sets the name property', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Eric');
         
-        expect(pet.name).toEqual('Fido');
+        expect(pet.name).toEqual('Eric');
     });
 
     it('has starting age of 0',() => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Eric');
 
         expect(pet.age).toEqual(0);
     });
@@ -20,10 +20,18 @@ describe('constructor', () => {
 
 describe('growUP', () => {
     it('increase age by 1',() => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Eric');
 
         pet.growUp();
 
         expect(pet.age).toEqual(1);
     });
+    it('increase hunger by 5',() => {
+        const pet = new Pet('Eric');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+
+    })
 });
--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,32 @@
 
-const pet = new Pet('Fido');
+const pet = new Pet('Eric');
+const initialHunger = 0
+const initialFitness = 10
+
 
 function Pet(name) {
     this.name = name; 
     this.age = 0;
-    this.growUp = function() {
-        this.age += 1;
-    };
+    this.hunger = 0;
+    this.fitness = 10;
 };
+
+Pet.prototype = {
+    get isAlive () {
+            const alive =  this.age < 30 && this.hunger < 10 && this.fitness > 0;
+            if (alive) { return true }
+            return false  
+    },     
+
+    growUp: function() {
+        if (!this.isAlive) {
+            throw new Error ('Your pet is no longer alive');
+        }
+
+        this.age += 1
+        this.hunger += 5
+        this.fitness -= 3
+    },
+}
 
 module.exports = Pet;
